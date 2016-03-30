dbac6b7c3f4044ae38e79d52f398c8b885293a3b
Bridget McMillan <bridgetmc09@gmail.com>
2016-03-30 17:35:27 -0400

    Yelp! API actual progress.

--- whatsForLunch/app/controllers/application_controller.rb
+++ whatsForLunch/app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+#below is suggested to be in HomeController... do we want
+#it here, or in UserController?
+
+  # def search
+  #   parameters = { term: params[:term], limit: 1 }
+  #   render json: Yelp.client.search(‘//location//’, parameters)
+  # end
+
+#***params[:term] I guess will be equal
+#to whatever array we wind up with?
 end
