00c99eebfcb0cebb8c9cad66a1aa7ac2c8cc1307
AlExcel <alexc1117@gmail.com>
2016-03-31 14:03:39 -0400

    fixed the error

--- whatsForLunch/app/models/user.rb
+++ whatsForLunch/app/models/user.rb
@@ -1,14 +1,8 @@
 class User < ActiveRecord::Base
 	# attr_accessible :location, :latitude, :longitude
 
-<<<<<<< HEAD
 	# reverse_geocoded_by :latitude, :longitude => :location
 	# after_validation :reverse_geocode
-=======
-# geocoded_by :ip_address,
-#   :latitude => :latitude, :longitude => :longitude
-# after_validation :geocode
->>>>>>> b8ac9b92468eeff268afae1fabf6659296c92ebd
 
 	# # @user.location = @user.city + ", " + @user.country
 end
