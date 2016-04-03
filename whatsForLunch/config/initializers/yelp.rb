acb8358e6338609314c72326dd3a4ed517ddf2e6
Bridget McMillan <bridgetmc09@gmail.com>
2016-04-03 14:45:26 -0400

    added commas

--- whatsForLunch/config/initializers/yelp.rb
+++ whatsForLunch/config/initializers/yelp.rb
@@ -1,10 +1,10 @@
 require 'yelp'
 
 client = Yelp::Client.new({
-          consumer_key: ENV['YELP_CONSUMER_KEY']
-          consumer_secret: ENV['YELP_CONSUMER_SECRET']
-          token: ENV['YELP_TOKEN']
-          token_secret: ENV['YELP_TOKEN_SECRET']
+          consumer_key: ENV['YELP_CONSUMER_KEY'],
+          consumer_secret: ENV['YELP_CONSUMER_SECRET'],
+          token: ENV['YELP_TOKEN'],
+          token_secret: ENV['YELP_TOKEN_SECRET'],
           })
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 # Yelp.client.search('', { term: 'food' })
